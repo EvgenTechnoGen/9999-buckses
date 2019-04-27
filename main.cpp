9b29acd3288c24ad3f11dfd733fc6385a4a7a509
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-04-27 23:43:08 +0300

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main () {
+    setlocale(LC_ALL, "Rus");
+
+    int sum;
+    std::cout << "Какую сумму вы желаете обналичить?" << std::endl;
+    std::cin >> sum;
