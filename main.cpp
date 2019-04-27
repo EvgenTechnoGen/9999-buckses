270d1cf03ad981bf06dd3a34433bb809948bf47d
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-04-27 23:43:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,3 +7,7 @@ int main () {
     int sum;
     std::cout << "Какую сумму вы желаете обналичить?" << std::endl;
     std::cin >> sum;
+
+    if (sum == 0 || sum > 9999) {
+        std::cout << "Число не попадает в диапозон от 1 до 9999!" << std::endl;
+    }
