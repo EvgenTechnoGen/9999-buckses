d65c59309a924aeb94f13d20c827935995ab679f
Evgen <46866157+EvgenTechnoGen@users.noreply.github.com>
2019-05-01 15:34:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,3 +11,13 @@ int main () {
     if (sum == 0 || sum > 9999) {
         std::cout << "Число не попадает в диапозон от 1 до 9999!" << std::endl;
     }
+    
+    if ((sum / 1000) % 10 == 1) std::cout << "Одна тысяча" << std::endl;
+    else if ((sum / 1000) % 10 == 2) std::cout << "Две тысячи" << std::endl;
+    else if ((sum / 1000) % 10 == 3) std::cout << "Три тысячи" << std::endl;
+    else if ((sum / 1000) % 10 == 4) std::cout << "Четыре тысячи" << std::endl;
+    else if ((sum / 1000) % 10 == 5) std::cout << "Пять тысяч" << std::endl;
+    else if ((sum / 1000) % 10 == 6) std::cout << "Шесть тысяч" << std::endl;
+    else if ((sum / 1000) % 10 == 7) std::cout << "Семь тысяч" << std::endl;
+    else if ((sum / 1000) % 10 == 8) std::cout << "Восемь тысяч" << std::endl;
+    else if ((sum / 1000) % 10 == 9) std::cout << "Девять тысяч" << std::endl;
