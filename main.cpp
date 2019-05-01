4473e335b9ff4a3ec5fc0b33a91668a5405e8775
Evgen <46866157+EvgenTechnoGen@users.noreply.github.com>
2019-05-01 15:34:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
 
+int main () {
+    setlocale(LC_ALL, "Rus");
+    
+    int sum;
+    std::cout << "Какую сумму вы желаете обналичить?" << std::endl;
+    std::cin >> sum;
+    
+    if (sum == 0 || sum > 9999) {
+        std::cout << "Число не попадает в диапозон от 1 до 9999!" << std::endl;
+    }
